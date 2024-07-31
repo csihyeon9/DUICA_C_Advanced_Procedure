b4329be1af83bd8f2de72ba3ae8378d7cda23832
Sihyun Cha <110538039+csihyeon9@users.noreply.github.com>
2024-07-31 12:18:40 +0900

    Update DAY5.Q1.c

--- C언어고급과정-DAY5/DAY5.Q1.c
+++ C언어고급과정-DAY5/DAY5.Q1.c
@@ -2,27 +2,23 @@
 
 #define MAX 160
 
-// 곱셈 함수
-void multiply(int result[], int *result_size, int num) {
-    int carry = 0; // 자리 올림
-    for (int i = 0; i < *result_size; i++) {
-        int prod = result[i] * num + carry;
-        result[i] = prod % 10; // 현재 자리의 값 저장
-        carry = prod / 10; // 자리 올림 값 저장
-    }
-    while (carry) {
-        result[(*result_size)++] = carry % 10;
-        carry /= 10;
-    }
-}
-
 // 팩토리얼 함수
 void factorial(int n) {
     int result[MAX] = {1}; // 0! = 1
     int result_size = 1; // 현재 결과의 자리 수
+    int carry, prod;
 
     for (int i = 2; i <= n; i++) {
-        multiply(result, &result_size, i);
+        carry = 0;
+        for (int j = 0; j < result_size; j++) {
+            prod = result[j] * i + carry;
+            result[j] = prod % 10; // 현재 자리의 값 저장
+            carry = prod / 10; // 자리 올림 값 저장
+        }
+        while (carry) {
+            result[result_size++] = carry % 10;
+            carry /= 10;
+        }
     }
 
     printf("%d! = ", n);
@@ -34,10 +30,9 @@ void factorial(int n) {
 
 int main() {
     int n;
-    printf("Enter a number (0-100): ");
-    scanf("%d", &n);
-    if (n < 0 || n > 100) {
-        printf("Please enter a number between 0 and 100.\n");
+    printf("0 ~ 100 입력: ");
+    if (scanf("%d", &n) != 1 || n < 0 || n > 100) {
+        printf("0 ~ 100을 입력하시오\n");
     } else {
         factorial(n);
     }
